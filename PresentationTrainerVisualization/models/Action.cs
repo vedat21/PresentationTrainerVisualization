23eded42c0b79394a9c0fc093e17e5c110461385
yildiz <vedat-yildiz@hotmail.de>
2023-04-07 18:01:10 +0200

    model update

---
+++ PresentationTrainerVisualization/models/Action.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationTrainerVisualization.models
+{
+    internal class Action
+    {
+        [JsonProperty("start")]
+        public string Start { get; set; }
+
+        [JsonProperty("end")]
+        public string End { get; set; }
+
+        [JsonProperty("id")]
+        public double Id { get; set; }
+
+        [JsonProperty("logAction")]
+        public string LogAction { get; set; }
+
+        [JsonProperty("mistake")]
+        public bool Mistake { get; set; }
+    }
+}
